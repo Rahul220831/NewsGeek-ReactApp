891351311470b7d7e7172fa1a5a9203b9e0625e9
Deadshot1711 <trippysoul0606@gmail.com>
2022-06-18 09:16:11 +0530

    First Commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+      let {title,description,imageUrl, newsUrl,author,date}= this.props;
+    return (
+      
+      <div className="my-2">
+        
+        <div className="card" >
+        
+  <img src={! imageUrl?"https://filecache.mediaroom.com/mr5mr_ibmnewsroom/193163/IBM%20Logo_Social.jpg": imageUrl} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{description}</p>
+    <p className="card-text"><small className="text-muted"> By: {!author?"Unknown": author} on {new Date(date).toGMTString()}</small></p>
+    <a rel=" noopener noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More </a>
+  </div>
+</div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
